e73f1f33b6457d87e082d19a16112a1d28f0ecdf
Aspirisha <nick-torrance@yandex.ru>
2014-10-26 15:34:00 +0300

    Добавлено ссаное яблочко

--- gen/com/autamncoding/stickman/R.java
+++ gen/com/autamncoding/stickman/R.java
@@ -22,7 +22,9 @@ public final class R {
         public static final int debug_font_size=0x7f040002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_sound_off=0x7f020000;
+        public static final int button_sound_on=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
@@ -37,6 +39,10 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int str_depth=0x7f050003;
+        public static final int str_start=0x7f050006;
+        public static final int str_toweb=0x7f050005;
+        public static final int str_university=0x7f050004;
     }
     public static final class style {
         /** 
--- src/com/autumncoding/stickman/MainActivity.java
+++ src/com/autumncoding/stickman/MainActivity.java
@@ -1,6 +1,6 @@
 package com.autumncoding.stickman;
 
-import com.autamncoding.stickman.R;
+import com.autamncoding.stickman.*;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -8,49 +8,32 @@ import android.graphics.Rect;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
-	GameHolder gameHolder;
+	
 	static int layout_height;
 	static int layout_width;
+	public static final int	VIEW_INTRO = 0;
+	public static final int	VIEW_GAME  = 1;
+	
+	int	m_viewCur = -1;
+	AppIntro m_app;
+	ViewIntro m_viewIntro;
+	GameHolder m_gameHolder;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		gameHolder = new GameHolder(this);
-		setContentView(gameHolder);
-		gameHolder.post(new Runnable() {
-
-			@Override
-			public void run() {
-				Rect rect = new Rect(); 
-	            Window win = getWindow();  // Get the Window
-	            win.getDecorView().getWindowVisibleDisplayFrame(rect); 
-	            // Get the height of Status Bar 
-	            int statusBarHeight = rect.top; 
-	            // Get the height occupied by the decoration contents 
-	            int contentViewTop = win.findViewById(Window.ID_ANDROID_CONTENT).getTop(); 
-	            // Calculate titleBarHeight by deducting statusBarHeight from contentViewTop  
-	            int titleBarHeight = contentViewTop - statusBarHeight; 
-	            Log.i("MY", "titleHeight = " + titleBarHeight + " statusHeight = " + statusBarHeight + " contentViewTop = " + contentViewTop); 
-	            
-	            DisplayMetrics metrics = new DisplayMetrics();
-	            getWindowManager().getDefaultDisplay().getMetrics(metrics);   
-	            int screenHeight = metrics.heightPixels;
-	            int screenWidth = metrics.widthPixels;
-	            Log.i("MY", "Actual Screen Height = " + screenHeight + " Width = " + screenWidth);   
-	 
-	            // Now calculate the height that our layout can be set
-	            // If you know that your application doesn't have statusBar added, then don't add here also. Same applies to application bar also 
-	            layout_height = screenHeight - (titleBarHeight + statusBarHeight);
-	            layout_width = screenWidth;
-	            Log.i("MY", "Layout Height = " + layout_height);   
-	            gameHolder.playingTable.setMetrics();
-			}
-		});
+		super.onCreate(savedInstanceState);		
+		 // Create application
+        m_app = new AppIntro(this, AppIntro.LANGUAGE_ENG);
+        // Create view
+        setView(VIEW_INTRO);
 	}
 
 	@Override
@@ -59,5 +42,65 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public void setView(int viewID)
+	{
+		if (m_viewCur == viewID)
+		{
+			Log.d("THREE", "setView: already set");
+			return;
+		}
+	
+		m_viewCur = viewID;
+		if (m_viewCur == VIEW_INTRO)
+		{
+	        m_viewIntro = new ViewIntro(this);
+	        setContentView(m_viewIntro);
+	        m_viewIntro.start();
+		}
+		if (m_viewCur == VIEW_GAME)
+		{
+			Log.d("THREE", "Switch to m_viewGame" );
+			m_gameHolder = new GameHolder(this);
+			m_gameHolder.post(new Runnable() {
+
+				@Override
+				public void run() {
+					Rect rect = new Rect(); 
+		            Window win = getWindow();  // Get the Window
+		            win.getDecorView().getWindowVisibleDisplayFrame(rect); 
+		            // Get the height of Status Bar 
+		            int statusBarHeight = rect.top; 
+		            // Get the height occupied by the decoration contents 
+		            int contentViewTop = win.findViewById(Window.ID_ANDROID_CONTENT).getTop(); 
+		            // Calculate titleBarHeight by deducting statusBarHeight from contentViewTop  
+		            int titleBarHeight = contentViewTop - statusBarHeight; 
+		            Log.i("MY", "titleHeight = " + titleBarHeight + " statusHeight = " + statusBarHeight + " contentViewTop = " + contentViewTop); 
+		            
+		            DisplayMetrics metrics = new DisplayMetrics();
+		            getWindowManager().getDefaultDisplay().getMetrics(metrics);   
+		            int screenHeight = metrics.heightPixels;
+		            int screenWidth = metrics.widthPixels;
+		            Log.i("MY", "Actual Screen Height = " + screenHeight + " Width = " + screenWidth);   
+		 
+		            // Now calculate the height that our layout can be set
+		            // If you know that your application doesn't have statusBar added, then don't add here also. Same applies to application bar also 
+		            layout_height = screenHeight - (titleBarHeight + statusBarHeight);
+		            layout_width = screenWidth;
+		            Log.i("MY", "Layout Height = " + layout_height);   
+		            m_gameHolder.playingTable.setMetrics();
+				}
+			});
+			setContentView(m_gameHolder);
+			//m_gameHolder.start();
+		}
+	}
+
+	
+	public AppIntro getApp()
+	{
+		return m_app;
+	}
+	
 
 }
